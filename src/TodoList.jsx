601f8a08ba00548b01f2ee1ef5afbdadf54ea85c
sharif-islam96403 <sharif.islam96403@gmail.com>
2024-08-19 01:01:34 +0600

    Configure React app for deployment to Github Pages

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,6 +1,6 @@
 import React , {useState} from "react";
 import './TodoList.css';
-import deleteIcon from './delete.svg'
+
 function TodoList(){
 
     const [ongoingTasks,setOngoingTasks] = useState([]);
